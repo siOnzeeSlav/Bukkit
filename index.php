8b06e5755f420f0ae74dc732b699942c7a3dc9ba
Nyqi QwiDer <Nyqi.QwiDer@gmail.com>
2013-06-13 19:38:23 +0200

    Návrhy..

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <html lang="cs">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>Czech CB Repository</title>
     </head>
     <body>
-        
+        <div>
+            Zde bude obsah
+        </div>
     </body>
 </html>
