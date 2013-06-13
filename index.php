3b2f04f34d60b5aeef89384f51fac6921b662e88
Vojtěch Ducháček <vojtech.duchacek@gmail.com>
2013-06-13 20:30:29 +0300

    Doladění detailů..

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <html lang="cs">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>eBukkit</title>
     </head>
     <body>
-        
+        Černokněžník je nejlepší!
     </body>
 </html>
